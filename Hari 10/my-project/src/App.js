c9f45d635188f03efdf14ea098dd518b6db258f7
anjasopo <anjasopo@gmail.com>
2022-09-27 19:28:39 +0700

    Latihan hari ke-12

--- Hari 10/my-project/src/App.js
+++ Hari 10/my-project/src/App.js
@@ -5,16 +5,51 @@ import axios from "axios";
 const App = () => {
   const [data, setData] = useState(null);
 
+  //CRUD DATA
+  const [input, setInput] = useState({
+    name: "",
+    // hobby: "",
+  });
+
+  const [fecthStatus, setFecthStatus] = useState(true);
+
   useEffect(() => {
-    axios
-      .get("http://backendexample.sanbercloud.com/api/contestants")
-      .then((res) => {
-        setData([...res.data]);
-      })
-      .catch((error) => {});
-  }, []);
+    if (fecthStatus === true) {
+      axios
+        .get("http://backendexample.sanbercloud.com/api/contestants")
+        .then((res) => {
+          setData([...res.data]);
+        })
+        .catch((error) => {});
+
+      setFecthStatus(false);
+    }
+  }, [fecthStatus, setFecthStatus]);
 
   console.log(data);
+
+  const handleInput = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    if (name === "name") {
+      setInput({ ...input, name: value });
+    }
+    // else if (name === "hobby") {
+    //   setInput({ ...input, hobby: value });
+    // }
+  };
+
+  const handleSumbit = (event) => {
+    event.preventDefault();
+    let { name } = input;
+    axios
+      .post("https://backendexample.sanbercloud.com/api/contestants", { name })
+      .then((res) => {
+        console.log(res);
+        setFecthStatus(true);
+      });
+  };
 
   return (
     <>
@@ -30,6 +65,14 @@ const App = () => {
             })}
         </ul>
       </div>
+
+      <p>FORM DATA</p>
+
+      <form onSubmit={handleSumbit}>
+        <span>Nama :</span>
+        <input onChange={handleInput} value={input.name} name="name" />
+        <input type={"submit"} />
+      </form>
     </>
   );
 };
